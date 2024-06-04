489b43bf2e37814c039073eb6d4b5405f4b6b204
Erick Iván Gómez Cruz <erick.gomez.crz@gmail.com>
2024-06-04 15:42:38 -0600

    adding esp32.py, rp2040 an vl53rat modified
    This version of VL53Rat.py works (the previous one was wrong) and also change the way the class return its values. rp2040.py implements the communication with the esp32 via UART and also add the code for the ESP32 to receive and print data.

--- VL53Rat.py
+++ VL53Rat.py
@@ -1,14 +1,12 @@
 from VL53L0X import VL53L0X
-from machine import Pin, I2C, Timer
+from machine import Pin, Timer
 from utime import sleep_ms
 
 class VL53Rat:
 	def __init__(self, i2c, address, pin, limit, numSen):
 		self.numSen = numSen
-		self.vl53l0x = None
-		self.address = address
 		self.i2c = i2c
-		self.xshut = Pin(Pin(pin, Pin.OUT, value = 0))
+		self.address = address
 		self.timer = Timer()
 		self.miliseg = 0
 		self.seg = 0
@@ -17,20 +15,21 @@ class VL53Rat:
 		self.hrs = 0
 		self.ind = None
 		self.limit = limit
-		
-	def getTime(self):
-		tup = time.localtime()
-		fecha = str(tup[0]) + '/' + str(tup[1]) + '/' + str(tup[2])
-		hora  = str(tup[3]) + ':' + str(tup[4]) + ':' + str(tup[5])
-		tiempo = [fecha, hora]
-		return tiempo
+		self.xshut = Pin(pin, Pin.OUT, value = 0)
+		self.sensor = None
+	#end def
 	
 	def init(self):
 		self.xshut.value(1)
-		sleep_ms(2)
-		self.vl53l0x = VL53L0X(self.i2c,self.adddress)
+		sleep_ms(1)
+		self.sensor = VL53L0X(self.i2c, self.address)
+		"""try:
+			self.sensor = VL53L0X(self.i2c, self.address)
+		except Exception as e:
+			print("Error initializing VL53L0X sensor:", e)"""
+	#end def
 	
-	def counter(self, self.timer):
+	def counter(self, timer):
 		self.miliseg += 1
 		if(self.miliseg == 1000):
 			self.seg += 1
@@ -40,21 +39,27 @@ class VL53Rat:
 				self.seg = 0
 				if (self.min == 60):
 					self.hrs += 1
-					self.minu = 0
-		
+					self.min = 0
+	#end def
+	
 	def getDistance(self):
-		self.vl53l0x.start()
-		self.distance = self.vl53l0x.read()
-		self.vl53l0x.stop()
-		if self.distance < self.limit*10 and self.ind != 'menor':
-			self.timer.init(freq = 1000, mode = Timer.PERIODIC, callback = self.counter())
+		self.sensor.start()
+		self.distance = self.sensor.read()
+		self.sensor.stop()
+		#print(self.distance)
+		if self.distance < (self.limit*10) and self.ind != 'menor':
+			self.timer.init(freq = 1000, mode = Timer.PERIODIC, callback = self.counter)
+			#print('hay presencia: '+str(self.distance))
 			self.ind = 'menor'
-		elif self.distance >= self.lim*10 and self.ind != 'mayor':
+		elif self.distance >= (self.limit*10) and self.ind != 'mayor':
 			self.timer.deinit()
+			#print('no hay presencia: '+str(self.distance))
 			self.ind = 'mayor'
+	#end def
 	
 	def returnInfo(self):
-		self.TopEnd = self.getTime()
-		info = 'Sensor ' +str(self.numSen)+ ' detected presence for '+str(self.hrs)+' : '+str(self.min)+' : '+str(self.seg)+' :'+str(self.miliseg)
-		self. miliseg, self.seg, self.min, self.hrs = 0, 0, 0, 0
+		info = f'{str(self.hrs)}:{str(self.min)}:{str(self.seg)}:{str(self.miliseg)}'
+		self.miliseg, self.seg, self.min, self.hrs = 0, 0, 0, 0
 		return info
+	#end def
+
--- rp2040.py
+++ rp2040.py
@@ -1,11 +1,16 @@
-from machine import  I2C, Timer, Pin
+from machine import  I2C, Timer, Pin, UART
 from VL53Rat import VL53Rat
 from time import sleep
+from utime import sleep_ms
 import time
 
 bottomEnd, topEnd = [], []
 timep, limit = 10, 15
 info = None
+data2send = []
+
+pin_tx = Pin(0, Pin.OUT)
+pin_rx = Pin(1, Pin.IN)
 
 def getTime():
 		tup = time.localtime()
@@ -13,28 +18,46 @@ def getTime():
 		hora  = str(tup[3]) + ':' + str(tup[4]) + ':' + str(tup[5])
 		tiempo = [fecha, hora]
 		return tiempo
-	
+#end def
+
 def getInfo(signal):
-	global info, bottomEnd, topEnd
+	global info, bottomEnd, topEnd, data2send
 	topEnd = getTime()
-	info = sensorUno.returnInfo()
-	print(info + ' between '+str(bottomEnd[0])+ ' at '+str(bottomEnd[1])+ ' and '+str(topEnd[0])+ ' at ' +str(topEnd[1]))
+	rat1, rat2 = sensorUno.returnInfo(), sensorDos.returnInfo()
+	data2send = f'{bottomEnd[0]}, {bottomEnd[1]}, {topEnd[0]}, {topEnd[1]}, {rat1}, {rat2}'
+	uart.write(data2send)
 	bottomEnd = topEnd
+#end def
 
 signal = Timer()
 I2C_bus = I2C(1, sda=Pin(2), scl = Pin(3), freq=400000)
+uart   = UART(0, baudrate=115200, tx=pin_tx, rx=pin_rx, timeout=1, timeout_char=1)
 sensorUno = VL53Rat(I2C_bus, 0x30, 28, limit, 1)
-sensorDos = VL53Rat(I2C_bus, 0x30, 29, limit, 2)
+sensorDos = VL53Rat(I2C_bus, 0x31, 29, limit, 2)
 
-if __name__ == '__main__':
+def main():
+	global bottomEnd
 	signal.init(period=timep*1000, mode=Timer.PERIODIC, callback=getInfo)
-	bottomEnd= getTime()
+	bottomEnd = getTime()
 	sensorUno.init()
-	
+	sensorDos.init()
 	while True:
 		sensorUno.getDistance()
+		sensorDos.getDistance()
+		#print('\n')
 		sleep(1)
+#end def
+
+if __name__ == '__main__':
+	try:
+		main()
+	except Exception as e:
+		print('--- Caught Exception ---')
+		import sys
+		sys.print_exception(e)
+		print('----------------------------')
 	
+
 
 
 
